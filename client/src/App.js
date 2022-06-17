c42e5086307c0b74625752d0949c6d6012e3bb07
tduncan14 <nbatre1@yahoo.com>
2022-06-17 16:30:35 -0700

    keep moving forward

--- client/src/App.js
+++ client/src/App.js
@@ -2,9 +2,18 @@
 
 function App() {
   return (
-    <div>
+    <div className="flex justify-center items-center h-screen space-y-5 flex-col">
+   
      
 
+        <input type="text" className="border-2 border-gray-500 py-2 px-5" placeholder="username" />
+
+        <input type="text" className="border-2 border-gray-500 py-2 px-5" placeholder="password" />
+
+
+
+        <button className="bg-blue-500 py-2 px-5 text-white">LOGIN</button>
+     
     </div>
   );
 }
