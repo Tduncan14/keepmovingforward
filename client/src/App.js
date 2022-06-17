bbf1f414411beccff0923c2148fa6498a8f18d67
tduncan14 <nbatre1@yahoo.com>
2022-06-17 16:50:25 -0700

    keep moving forward

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom'
+import Register from './pages/Register';
+import Home from './pages/Home';
+import Login from '.pages/Login';
 
 
 function App() {
   return (
-    <div className="flex justify-center items-center h-screen space-y-5 flex-col">
-   
-     
+    <BrowserRouter>
+    
+    
 
-        <input type="text" className="border-2 border-gray-500 py-2 px-5" placeholder="username" />
+    <Routes>
+        <Route path ="/" element={Home} />
+        <Route path="/login" element={Login} />
+        <Route path="/register" element={Register} />
 
-        <input type="text" className="border-2 border-gray-500 py-2 px-5" placeholder="password" />
-
-
-
-        <button className="bg-blue-500 py-2 px-5 text-white">LOGIN</button>
-     
-    </div>
+    </Routes>
+    
+    
+    </BrowserRouter>
   );
 }
 
---
+++ client/src/pages/Login.js
@@ -0,0 +1,18 @@
+import React from 'react';
+
+
+
+const Login = () => {
+
+
+
+
+
+
+
+
+
+}
+
+
+export default Login
